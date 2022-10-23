21ee2750e31436fe648cce4d3b6a940ede28fb5e
Hunter Lawson <hunter.lawson@ufl.edu>
2022-10-23 19:30:04 -0400

    fix whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,9 @@
 //! that only the correct number of inputs and only valid options are passed
 //! into your functions. Climb will also generate help menus for your
 //! application and commands.
-//! 
+//!
 //! This is an example calculator application "`cool_calc`"" that was created with
-//! climb. For a walkthrough of how to create this example application, check out the 
+//! climb. For a walkthrough of how to create this example application, check out the
 //! README on the [Climb page on crates.io](https://crates.io/crates/climb).
 //!
 //! ```text
@@ -29,30 +29,30 @@
 //!
 //! Run `cool_calc [COMMAND] --help` to see help information for a specific command
 //! ```
-//! 
+//!
 //! ```text
 //! $ cool_calc add 10 20
-//! 
+//!
 //! 30
 //! ```
-//! 
+//!
 //! ```text
 //! $ cool_calc div --round 100 3
-//! 
+//!
 //! 33
 //! ```
-//! 
+//!
 //! ```text
 //! $ cool_calc div --help
-//! 
+//!
 //! Divide two numbers
 //! USAGE:
 //!         cool_calc div [OPTIONS] <NUMBER_A> <NUMBER_B>
-//! 
+//!
 //! ARGS:
 //!         <NUMBER_A>
 //!         <NUMBER_B>
-//! 
+//!
 //! OPTIONS:
 //!         -h--help                     Print help information
 //!             --round                  Round the result
